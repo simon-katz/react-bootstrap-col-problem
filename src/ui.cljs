ac02f439b28e6135e47294ea264b5b39b16679ba
Simon Katz <sk@nomistech.com>
2021-10-16 13:28:13 +0100

    Remove `:js-options {:entry-keys ["module" "browser" "main"]}`

--- src/ui.cljs
+++ src/ui.cljs
@@ -4,8 +4,6 @@
    ;; These are OK:
    ["react-bootstrap/Card"]
    ["react-bootstrap/Row"]
-   ;; This was failing with an error, but OK after adding `:js-options` in
-   ;; shadow-cljs.edn
    ["react-bootstrap/Col"]))
 
 (defn ^:dev/after-load render []
